2717970c9543f4d56c483b9723f7a5aeb2e5c241
Daijiro Wachi <daijiro.wachi@gmail.com>
2014-03-18 01:09:44 +0900

    Separate master / gh-pages

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,14 @@
 module.exports = function(grunt) {
   'use strict';
+
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-yuidoc');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-gh-pages');
+  grunt.loadNpmTasks('grunt-open');
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     yuidoc: {
@@ -10,8 +19,31 @@ module.exports = function(grunt) {
         url: '<%= pkg.homepage %>',
         options: {
           paths: 'src',
-          outdir: 'api'
+          outdir: 'dest/api'
         }
+      }
+    },
+    copy: {
+      all: {
+        expand: true,
+        cwd: 'src/',
+        src: ['*.js'],
+        dest: 'dest/assets/js/'
+      }
+    },
+    connect: {
+      dest: {
+        options: {
+          hostname: '0.0.0.0',
+          port: 5455,
+          base: 'dest/',
+          livereload: true
+        }
+      }
+    },
+    open: {
+      dest: {
+        path: 'http://localhost:5455'
       }
     },
     jshint: {
@@ -20,11 +52,37 @@ module.exports = function(grunt) {
         sub: true,
       },
       all: ['Gruntfile.js', 'src/css-filter.js']
+    },
+    'gh-pages': {
+      options: {
+        base: 'dest',
+      },
+      src: ['**']
+    },
+    watch: {
+      all: {
+        options: {
+          livereload: true,
+          hostname: 'localhost',
+          port: 5455
+        },
+        files: ['src/*.js'],
+        tasks: ['build'],
+      }
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-yuidoc');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.registerTask('test', ['jshint', 'yuidoc']);
-  grunt.registerTask('default', ['test']);
+  grunt.registerTask('build', ['test', 'copy']);
+  grunt.registerTask('server', ['connect', 'open']);
+
+  // Deploy gh-pages
+  grunt.registerTask('deploy', [
+    'build',
+    'gh-pages'
+  ]);
+  // Main task
+  grunt.registerTask('default', [
+    'build', 'server', 'watch'
+  ]);
 };
