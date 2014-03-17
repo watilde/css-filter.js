178405d6695a4d0d3f2fefaba9c608605dc98db1
Daijiro Wachi <daijiro.wachi@gmail.com>
2014-03-18 01:09:58 +0900

    Remove deleted files

--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  var elem = document.getElementById('cover-heading_input');
-  elem.addEventListener('change', function () {
-    cssFilter('#cat').blur(this.value + 'px');
-  });
-  cssFilter('#cat').blur('5px');
-}());
